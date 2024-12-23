d760af7474d449412b3b67de8ecf36488a686efe
upkishan11 <rockykishan11@gmail.com>
2024-12-23 09:10:49 +0530

    Update kish.js

--- kish.js
+++ kish.js
@@ -1,1 +1,23 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Products from './pages/Products';
+import About from './pages/About';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
+function App() {
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
